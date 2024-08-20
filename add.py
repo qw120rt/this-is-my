7da851325d5f2d5b28c068194c9dd16a0b32895b
qw120rt <xyj13789@163.com>
2024-08-20 21:26:31 +0800

    第一次

---
+++ add.py
@@ -0,0 +1,23 @@
+import turtle
+
+def draw_petal():
+    """
+    绘制一个花瓣形状。
+    
+    Args:
+        无参数。
+    
+    Returns:
+        无返回值。
+    
+    """
+    turtle.circle(100, 60)
+    turtle.left(120)
+    turtle.circle(100, 60)
+    turtle.left(120)
+
+for _ in range(6):
+    draw_petal()
+    turtle.right(60)
+
+turtle.done()
